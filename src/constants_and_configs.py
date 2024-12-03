32740da1369652b6d536ec8583b6f7bb8be2668b
VLMedvedev <c066en@gmail.com>
2024-12-03 20:47:26 +0100

    settings and oled

--- src/constants_and_configs.py
+++ src/constants_and_configs.py
@@ -5,27 +5,31 @@ from micropython import const
 
 
 class WiFi_mode(object):
-    WiFi_OFF = const(0)
-    WiFi_AP = const(1)
-    WiFi_client = const(2)
+    def __init__(self):
+        self.WiFi_OFF = const(0)
+        self.WiFi_AP = const(1)
+        self.WiFi_client = const(2)
 
 class Rele_control_mode(object):
-    BATTERY_LEVEL = const(0)
-    ALWAYS_OFF = const(1)
-    ALWAYS_ON = const(2)
+    def __init__(self):
+        self.BATTERY_LEVEL = const(0)
+        self.ALWAYS_OFF = const(1)
+        self.ALWAYS_ON = const(2)
 
 class View_mode(object):
-    BATTERY_LEVEL = const(0)
-    VIEW_INFO = const(1)
-    SETTING_UP = const(2)
-    SETTING_DOWN = const(3)
-    VIEW_OFF = const(4)
-    VIEW_ON = const(6)
+    def __init__(self):
+        self.BATTERY_LEVEL = const(0)
+        self.VIEW_INFO = const(1)
+        self.SETTING_UP = const(2)
+        self.SETTING_DOWN = const(3)
+        self.VIEW_OFF = const(4)
+        self.VIEW_ON = const(6)
 
 class HW_Config(object):
-    LED_PIN = const(15)
-    RELE_PIN = const(16)
-    CAN_RX_PIN = const(37)
-    CAN_TX_PIN = const(39)
+    def __init__(self):
+        self.LED_PIN = const(15)
+        self.RELE_PIN = const(16)
+        self.CAN_RX_PIN = const(37)
+        self.CAN_TX_PIN = const(39)
 
 
