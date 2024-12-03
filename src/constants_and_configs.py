a0cc44687d41efc29dfa70b7e4fdbfb78c08eb4f
VLMedvedev <c066en@gmail.com>
2024-12-03 20:28:00 +0100

    settings and oled

--- src/constants_and_configs.py
+++ src/constants_and_configs.py
@@ -1,27 +1,30 @@
-from enum import Enum
+#from enum import Enum
 
-class WiFi_mode(Enum):
-    WiFi_OFF = 0
-    WiFi_AP = 1
-    WiFi_client = 2
+#from thonny.plugins.microbit.api_stubs.micropython import const
 
-class Rele_control_mode(Enum):
-    BATTERY_LEVEL = 0
-    ALWAYS_OFF = 1
-    ALWAYS_ON = 2
 
-class View_mode(Enum):
-    BATTERY_LEVEL = 0
-    VIEW_INFO = 1
-    SETTING_UP = 2
-    SETTING_DOWN = 3
-    VIEW_OFF = 4
-    VIEW_ON = 6
+class WiFi_mode():
+    WiFi_OFF = const(0)
+    WiFi_AP = const(1)
+    WiFi_client = const(2)
 
-class HW_Config(object):
-    LED_PIN = 15
-    RELE_PIN = 16
-    CAN_RX_PIN = 37
-    CAN_TX_PIN = 39
+class Rele_control_mode():
+    BATTERY_LEVEL = const(0)
+    ALWAYS_OFF = const(1)
+    ALWAYS_ON = const(2)
+
+class View_mode():
+    BATTERY_LEVEL = const(0)
+    VIEW_INFO = const(1)
+    SETTING_UP = const(2)
+    SETTING_DOWN = const(3)
+    VIEW_OFF = const(4)
+    VIEW_ON = const(6)
+
+class HW_Config():
+    LED_PIN = const(15)
+    RELE_PIN = const(16)
+    CAN_RX_PIN = const(37)
+    CAN_TX_PIN = const(39)
 
 
