c6ce3a29a9737fdd3cfbe64e8242f66bfda3d001
VLMedvedev <c066en@gmail.com>
2024-12-07 11:01:52 +0100

    settings and oled

---
+++ src/battery_reader.py
@@ -0,0 +1,29 @@
+#import random
+import esp32_soc
+import constants_and_configs as cons
+#import machine
+import asyncio
+import utime
+
+class Battery_reader():
+    def __init__(self):
+        ret = esp32_soc.driver_init(cons.HW_CAN_RX_PIN, cons.HW_CAN_TX_PIN)
+        print(f" driver init  {ret}")
+        self.soc_level = 123
+        self.non_stop = True
+        self.task = asyncio.create_task(self.run())
+
+    async def run(self):
+        while self.non_stop:
+            self.soc_level = await esp32_soc.read_soc_level(0)
+            await asyncio.sleep_ms(1000)
+
+    def get_soc_level(self):
+        return self.soc_level
+
+    def stop(self):
+        self.non_stop = False
+
+if __name__ == "__main__":
+    br = Battery_reader()
+    br.run()
