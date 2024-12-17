21fbca0f53862742b0e77ce5e6a91c4988fa5959
VLMedvedev <c066en@gmail.com>
2024-12-17 20:06:59 +0100

    ugit prepare

--- boot.py
+++ boot.py
@@ -10,14 +10,14 @@ print(_SYSNAME)
 #if wm.is_connected():
 #    micropython_OTA.main()
 import ugit
-wifi_conf_dict = {"ssid": "A1-C4A220", "password": "7KBBBLX7FQ"}
-wlan = ugit.wificonnect(wifi_conf_dict.get('ssid'),
+#wifi_conf_dict = {"ssid": "A1-C4A220", "password": "7KBBBLX7FQ"}
+#wlan = ugit.wificonnect(wifi_conf_dict.get('ssid'),
                         wifi_conf_dict.get('password'))
 
-ugit.update()
+#ugit.update()
 # backup internal files
-ugit.backup() # saves to ugit.backup file
+#ugit.backup() # saves to ugit.backup file
 # Pull single file
 #ugit.pull('file_name.ext','Raw_github_url')
 # Pull all files
-ugit.pull_all()
+#ugit.pull_all()
