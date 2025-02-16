8f0b85d7547a5ccfa59af69ae9aa7e888605970e
VLMedvedev <c066en@gmail.com>
2025-02-16 21:47:49 +0100

    commander

--- app/main.py
+++ app/main.py
@@ -20,6 +20,7 @@ rele_mode = RELE_BATTERY_LEVEL
 f_rele_is_on = False
 soc_level = 50
 screen_timer = 0
+settings_mode = True
 
 from mp_commander import (set_level_to_config_file,
                           set_rele_mode_to_config_file,
@@ -32,12 +33,12 @@ pin_rele = Pin(HW_RELE_PIN, Pin.OUT, value=0)
 pin_led = Pin(HW_LED_PIN, Pin.OUT, value=1)
 
 async def can_processing():
-    global soc_level, off_level, on_level, rele_mode, f_rele_is_on, screen_timer
+    global soc_level, off_level, on_level, rele_mode, f_rele_is_on, settings_mode
     can_init()
     time.sleep(3)
     can_id_scan()
     while True:
-        if screen_timer == 0:
+        if not settings_mode:
             soc_level = await can_soc_read()
             try:
                 soc_level = int(soc_level)
@@ -51,14 +52,14 @@ async def can_processing():
                                                                                    soc_level)
                 if f_change_rele_state:
                     set_rele_on_off(pin_rele, f_rele_is_on)
-        elif screen_timer == 5:
-            broker.publish(TOPIC_COMMAND_VIEW_MODE, VIEW_MODE_SETTINGS)
+        # elif screen_timer == 5:
+        #     broker.publish(TOPIC_COMMAND_VIEW_MODE, VIEW_MODE_SETTINGS)
         else:
             soc_level = 123
         await asyncio.sleep(CAN_SOC_CHECK_PERIOD_SEC)
 
 async def controller_processing():
-    global off_level, on_level, rele_mode, f_rele_is_on
+    global off_level, on_level, rele_mode, f_rele_is_on, screen_timer, settings_mode
     logging.info("[controller_processing]")
     queue = RingbufQueue(20)
     broker.subscribe(TOPIC_COMMAND_WIFI_MODE, queue)
@@ -66,6 +67,7 @@ async def controller_processing():
     broker.subscribe(TOPIC_COMMAND_LEVEL_DOWN, queue)
     broker.subscribe(TOPIC_COMMAND_LEVEL_UP, queue)
     async for topic, message in queue:
+        settings_mode = True
         logging.info(f"topic {topic}, message {message}")
         if (topic == TOPIC_COMMAND_LEVEL_UP or topic == TOPIC_COMMAND_LEVEL_DOWN):
             file_config_name = "app_config"
@@ -82,7 +84,7 @@ async def controller_processing():
         await asyncio.sleep(0.1)
 
 async def start_screen_timer():
-    global screen_timer
+    global screen_timer, settings_mode
     logging.info("[start_screen_timer]")
     while True:
         await asyncio.sleep(1)
@@ -91,6 +93,7 @@ async def start_screen_timer():
             if screen_timer < 0:
                 logging.info("redraw screen...")
                 broker.publish(TOPIC_COMMAND_VIEW_MODE, VIEW_MODE_RELE_SOC_AUTO)
+                settings_mode = False
 
 async def start_oled_display():
     global soc_level, off_level, on_level, rele_mode, f_rele_is_on, screen_timer
@@ -104,16 +107,12 @@ async def start_oled_display():
         logging.info(f"topic {topic}, message {view_mode}")
         if view_mode == VIEW_MODE_SETTING_DOWN_OFF_LEVEL:
             oled.draw_setting_level(off_level, button_group="down")
-            screen_timer = SCREEN_TIMER_SEC
         elif view_mode == VIEW_MODE_SETTING_DOWN_ON_LEVEL:
             oled.draw_setting_level(on_level, button_group="down")
-            screen_timer = SCREEN_TIMER_SEC
         elif view_mode == VIEW_MODE_SETTING_UP_OFF_LEVEL:
             oled.draw_setting_level(off_level, button_group="up")
-            screen_timer = SCREEN_TIMER_SEC
         elif view_mode == VIEW_MODE_SETTING_UP_ON_LEVEL:
             oled.draw_setting_level(on_level, button_group="up")
-            screen_timer = SCREEN_TIMER_SEC
         elif view_mode == VIEW_MODE_RELE_SOC_AUTO:
             oled.draw_charge_level(soc_level, f_rele_is_on)
         elif view_mode == VIEW_MODE_RELE_OFF:
@@ -122,16 +121,12 @@ async def start_oled_display():
             oled.draw_on()
         elif view_mode == VIEW_MODE_WIFI_OFF_INFO:
             oled.view_info(WIFI_MODE_OFF)
-            screen_timer = SCREEN_TIMER_SEC
         elif view_mode == VIEW_MODE_WIFI_AP_INFO:
             oled.view_info(WIFI_MODE_AP)
-            screen_timer = SCREEN_TIMER_SEC
         elif view_mode == VIEW_MODE_WIFI_CLI_INFO:
             oled.view_info(WIFI_MODE_CLIENT)
-            screen_timer = SCREEN_TIMER_SEC
         elif view_mode == VIEW_MODE_SETTINGS:
             oled.view_settings()
-            screen_timer = SCREEN_TIMER_SEC
 
 # Coroutine: entry point for asyncio program
 async def main():
