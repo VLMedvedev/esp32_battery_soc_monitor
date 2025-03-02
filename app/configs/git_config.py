fe62a5d8cfe2d768fc68a552475e440e38f745ad
VLMedvedev <c066en@gmail.com>
2025-03-02 17:10:34 +0100

    msgid

--- app/configs/git_config.py
+++ app/configs/git_config.py
@@ -1,9 +1,9 @@
 GITHUB_OWNER = 'VLMedvedev'
 GITHUB_REPOSITORY = 'esp32_battery_soc_monitor'
-GITHUB_BRANCH = 'main'
+GITHUB_BRANCH = 'stable'
 GITHUB_APP_FOLDER = 'app'
 GITHUB_TOKEN = ""
-EXCLUDE_CONFIG_FILES = True
+EXCLUDE_CONFIG_FILES = False
 EXCLUDE_LIST = "sha1_internal_save_file.json"
 AUTO_UPDATE_FROM_GIT = True
 REBUILD_SHA1_INTERNAL_FILE = True
