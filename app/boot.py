d533ef0113c52d5efdaed6315716f671bc155a22
VLMedvedev <c066en@gmail.com>
2025-03-02 15:55:10 +0100

    git configs

--- app/boot.py
+++ app/boot.py
@@ -2,22 +2,19 @@
 # to minimise memory fragmentation as we sometimes want to
 # allocate relatively large blocks of ram.
 import gc
-from phew import connect_to_wifi
-
+from configs.sys_config import AUTO_START_WEBREPL, AUTO_CONNECT_TO_WIFI_AP
+from wifi_ap.wifi_portal import connect_to_wifi_ap
 """Main function. Runs after board boot, before main.py
 Connects to Wi-Fi and checks for latest git version.
 """
-
-#if AUTO_CONNECT_TO_WIFI_AP:
-ip_address = connect_to_wifi_ap()
-
-if AUTO_START_WEBREPL:
-    #logging.info("[AUTO_START_WEBREPL]")
-    import webrepl
-    # #asyncio.create_task(webrepl.start())
-    f_start_loop = False
-    webrepl.start()
-
+if AUTO_CONNECT_TO_WIFI_AP:
+    ip_address = connect_to_wifi_ap()
+    if AUTO_START_WEBREPL:
+        #logging.info("[AUTO_START_WEBREPL]")
+        import webrepl
+        # #asyncio.create_task(webrepl.start())
+        f_start_loop = False
+        webrepl.start()
 
 #gc.set_threshold(50000)
 gc.collect()
