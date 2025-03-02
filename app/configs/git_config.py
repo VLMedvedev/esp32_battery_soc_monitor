fbbab8635e1ee3c4ce7ba830925542ba8acb7e0e
VLMedvedev <c066en@gmail.com>
2025-03-02 19:26:11 +0100

    msgid

--- app/configs/git_config.py
+++ app/configs/git_config.py
@@ -4,6 +4,6 @@ GITHUB_BRANCH = 'main'
 GITHUB_APP_FOLDER = 'app'
 GITHUB_TOKEN = ""
 EXCLUDE_CONFIG_FILES = False
-EXCLUDE_LIST = "sha1_internal_save_file.json"
+EXCLUDE_LIST = "sha1_internal_save_file.json,reboot_counter.txt"
 AUTO_UPDATE_FROM_GIT = True
 REBUILD_SHA1_INTERNAL_FILE = True
